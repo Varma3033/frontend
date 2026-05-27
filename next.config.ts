724b05cc073f168e29ca348456609a8ec33c59da
Varma3033 <absvarma33@gmail.com>
2026-05-28 00:05:57 +1000

    images?

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-    { 
-      protocol: "https",
-      hostname: "exciting-gift-fd9a4caa36.strapiapp.com",
-      pathname: "/uploads/**",
-    },
-   ],
+      {
+        protocol: "https",
+        hostname: "exciting-gift-fd9a4caa36.media.strapiapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "exciting-gift-fd9a4caa36.strapiapp.com",
+        pathname: "/uploads/**",
+      },
+    ],
   },
 };
 
